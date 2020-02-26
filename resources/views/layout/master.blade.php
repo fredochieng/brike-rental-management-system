3a7479d2e62e528bcae5b19512c58f99a5faa74c
fredochieng <fredrick.owuor2014@gmail.com>
2020-02-27 00:27:31 +0300

    Duplicate datatable plugin error fixed

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -204,22 +204,6 @@
         <script src="{{ asset('assets/js/pages/tables/jquery-datatable.js') }}"></script>
         @endif
 
-        @if (Request::segment(2) === 'property-list' || Request::segment(2) === 'categories' || Request::segment(2) ===
-        'variations' || Request::segment(2) === 'tenants-list' || Request::segment(2) === 'rooms-list' ||
-        Request::segment(2) === 'room-assignments' || Request::segment(2) === 'room-adjustments' || Request::segment(2)
-        === 'payments'
-
-        )
-        <script src="{{ asset('assets/bundles/datatablescripts.bundle.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-datatable/buttons/dataTables.buttons.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-datatable/buttons/buttons.bootstrap4.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-datatable/buttons/buttons.colVis.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-datatable/buttons/buttons.html5.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/jquery-datatable/buttons/buttons.print.min.js') }}"></script>
-        <script src="{{ asset('assets/vendor/sweetalert/sweetalert.min.js') }}"></script>
-        <script src="{{ asset('assets/js/pages/tables/jquery-datatable.js') }}"></script>
-        @endif
-
         @if (Request::segment(2) === 'variations')
         <script src="{{ asset('/js/addVariation.js') }}"></script>
         @endif
