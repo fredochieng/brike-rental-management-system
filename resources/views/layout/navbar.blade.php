196f8f8f0a7f79255b04fd2002da6ed59a735dab
fredochieng <fredrick.owuor2014@gmail.com>
2020-03-04 12:30:09 +0300

    Nav bar quick links modified

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -21,87 +21,23 @@
             <div id="navbar-menu">
                 <ul class="nav navbar-nav">
                     <li class="dropdown">
-                        <a href="javascript:void(0);" class="dropdown-toggle icon-menu" data-toggle="dropdown">
-                            <i class="icon-bell"></i>
-                            <span class="notification-dot"></span>
-                        </a>
-                        <ul class="dropdown-menu notifications">
-                            <li class="header"><strong>You have 4 new Notifications</strong></li>
-                            <li>
-                                <a href="javascript:void(0);">
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <i class="icon-info text-warning"></i>
-                                        </div>
-                                        <div class="media-body">
-                                            <p class="text">Campaign <strong>Holiday Sale</strong> is nearly reach
-                                                budget limit.</p>
-                                            <span class="timestamp">10:00 AM Today</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);">
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <i class="icon-like text-success"></i>
-                                        </div>
-                                        <div class="media-body">
-                                            <p class="text">Your New Campaign <strong>Holiday Sale</strong> is approved.
-                                            </p>
-                                            <span class="timestamp">11:30 AM Today</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);">
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <i class="icon-pie-chart text-info"></i>
-                                        </div>
-                                        <div class="media-body">
-                                            <p class="text">Website visits from Twitter is 27% higher than last week.
-                                            </p>
-                                            <span class="timestamp">04:00 PM Today</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0);">
-                                    <div class="media">
-                                        <div class="media-left">
-                                            <i class="icon-info text-danger"></i>
-                                        </div>
-                                        <div class="media-body">
-                                            <p class="text">Error on website analytics configurations</p>
-                                            <span class="timestamp">Yesterday</span>
-                                        </div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="footer"><a href="javascript:void(0);" class="more">See all notifications</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
                         <a href="javascript:void(0);" class="dropdown-toggle icon-menu" data-toggle="dropdown"><i
                                 class="icon-equalizer"></i></a>
                         <ul class="dropdown-menu user-menu menu-icon">
-                            <li class="menu-heading">ACCOUNT SETTINGS</li>
-                            <li><a href="javascript:void(0);"><i class="icon-note"></i> <span>Basic</span></a></li>
-                            <li><a href="javascript:void(0);"><i class="icon-equalizer"></i>
-                                    <span>Preferences</span></a></li>
-                            <li><a href="javascript:void(0);"><i class="icon-lock"></i> <span>Privacy</span></a></li>
-                            <li><a href="javascript:void(0);"><i class="icon-bell"></i> <span>Notifications</span></a>
+                            <li class="menu-heading">QUICK LINKS</li>
+                            <li><a href="{{route('rent.payments')}}"><i class="icon-note"></i> <span>Payments</span></a>
                             </li>
-                            <li class="menu-heading">BILLING</li>
-                            <li><a href="javascript:void(0);"><i class="icon-credit-card"></i> <span>Payments</span></a>
+                            <li><a href="{{route('assignments.room-assignments')}}"><i class="icon-equalizer"></i>
+                                    <span>Room Assignments</span></a></li>
+
+                            <li><a href="{{route('rent-payments.trackers')}}"><i class="icon-lock"></i> <span>Rent
+                                        Payment
+                                        Trackers</span></a></li>
+                            <li><a href="{{route('messages.sms-list')}}"><i class="icon-envelope"></i> <span>Bulk
+                                        SMS</span></a>
                             </li>
-                            <li><a href="javascript:void(0);"><i class="icon-printer"></i> <span>Invoices</span></a>
-                            </li>
-                            <li><a href="javascript:void(0);"><i class="icon-refresh"></i> <span>Renewals</span></a>
+                            <li><a href="{{route('messages.sms-list')}}"><i class="icon-user"></i> <span>Profile
+                                    </span></a>
                             </li>
                         </ul>
                     </li>
